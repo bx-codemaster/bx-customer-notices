143427769fd17e76c9680a7e7dfd654ee7cf08e4
bx-codemaster <axel.benkert@online-power.de>
2026-06-16 09:30:59 +0200

    Hinweis von noRiddle

--- src/includes/extra/header/header_body/bx_customer_notices.php
+++ src/includes/extra/header/header_body/bx_customer_notices.php
@@ -1,12 +1,13 @@
 <?php
-// BOF - Timo Paul (mail[at]timopaul[dot]biz) - 2014-06-22 - customersNotice
-$customerNoticesManagerFile = DIR_FS_EXTERNAL . 'customer_notices/classes/CustomerNoticesManager.class.php';
-if (!class_exists('CustomerNoticesManager') && file_exists($customerNoticesManagerFile)) {
-  require_once $customerNoticesManagerFile;
-}
+if (defined('MODULE_BX_CUSTOMER_NOTICES_STATUS') && 'True' == MODULE_BX_CUSTOMER_NOTICES_STATUS) {
+  // BOF - Timo Paul (mail[at]timopaul[dot]biz) - 2014-06-22 - customersNotice
+  $customerNoticesManagerFile = DIR_FS_EXTERNAL . 'customer_notices/classes/CustomerNoticesManager.class.php';
+  if (!class_exists('CustomerNoticesManager') && file_exists($customerNoticesManagerFile)) {
+    require_once $customerNoticesManagerFile;
+  }
 
-if (class_exists('CustomerNoticesManager')) {
-  CustomerNoticesManager::run();
+  if (class_exists('CustomerNoticesManager')) {
+    CustomerNoticesManager::run();
+  }
+  // EOF - Timo Paul (mail[at]timopaul[dot]biz) - 2014-06-22 - customersNotice
 }
-// EOF - Timo Paul (mail[at]timopaul[dot]biz) - 2014-06-22 - customersNotice
-?>
